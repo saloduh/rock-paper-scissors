52fef9ed5d453083b15101e8ff4a4ea9db0d5a0f
saloduh <36436025+saloduh@users.noreply.github.com>
2023-10-19 23:08:41 -0700

    Update rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,8 @@
-
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase{};
+  if {userInput === 'rock'|| userInput === 'scissors' || userInput === 'paper'} {
+        return userInput;
+  } else {
+    console.log{'Error, please type: rock, paper or scissors,'};
+  }
+}
